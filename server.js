365ea1c0145c2f585a14bc60953e3dfbc1a98341
dhanushramudri <ramudridhanush@gmail.com>
2025-01-01 18:15:55 +0530

    add image limit

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+// Increase payload size limits
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 // Updated CORS configuration
 app.use(
   cors({
